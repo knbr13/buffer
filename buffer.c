b580062dc7f716c9dc360537495e5edf42ee53d2
knbr13 <abdullahalaadine63@gmail.com>
2025-03-20 17:04:58 +0200

    define and implement 'Buffer* buffer_create_with_size(size_t initial_size)' func

--- buffer.c
+++ buffer.c
@@ -25,3 +25,23 @@ Buffer* buffer_create() {
 
     return buf;
 }
+
+Buffer* buffer_create_with_size(size_t initial_size) {
+    Buffer* buf = (Buffer*)malloc(sizeof(Buffer));
+    if(!buf) {
+        return NULL;
+    }
+
+    buf->len = 0;
+    if (initial_size == 0) {
+        initial_size = smallBufferSize;
+    }
+    buf->cap = initial_size;
+    buf->data = (char*)malloc(initial_size);
+    if (!buf->data) {
+        free(buf);
+        return NULL;
+    }
+
+    return buf;
+}
--- buffer.h
+++ buffer.h
@@ -1,8 +1,11 @@
 #ifndef BUFFER_H
 #define BUFFER_H
 
+#include <stdio.h>
+
 typedef struct Buffer Buffer;
 
 Buffer* buffer_create();
+Buffer* buffer_create_with_size(size_t initial_size);
 
 #endif
